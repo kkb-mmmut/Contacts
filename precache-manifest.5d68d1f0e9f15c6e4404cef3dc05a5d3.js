2e1991e1625fbae74b5dd9413d31ea4827054008
Kamlesh Kumar Bind <kkb.mmmut@gmail.com>
2023-02-17 17:44:56 +0530

    Updates

---
+++ precache-manifest.5d68d1f0e9f15c6e4404cef3dc05a5d3.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5e237db1f9682df40a63cfe0f67fbed6",
+    "url": "/Contacts/index.html"
+  },
+  {
+    "revision": "6fab00ec9feb6064918a",
+    "url": "/Contacts/static/css/2.3ec6da42.chunk.css"
+  },
+  {
+    "revision": "7e8adb329509f4b02583",
+    "url": "/Contacts/static/css/main.0244e6d3.chunk.css"
+  },
+  {
+    "revision": "6fab00ec9feb6064918a",
+    "url": "/Contacts/static/js/2.f634bbf9.chunk.js"
+  },
+  {
+    "revision": "35f1a3ca561e93b64bcba705f76bd30f",
+    "url": "/Contacts/static/js/2.f634bbf9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7e8adb329509f4b02583",
+    "url": "/Contacts/static/js/main.052e85b3.chunk.js"
+  },
+  {
+    "revision": "4e0e34f265fae8f33b01b27ae29d9d6f",
+    "url": "/Contacts/static/js/main.052e85b3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a07a47a7a96428f19873",
+    "url": "/Contacts/static/js/runtime-main.622d7853.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/Contacts/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/Contacts/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/Contacts/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/Contacts/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/Contacts/static/media/fontawesome-webfont.fee66e71.woff"
+  }
+]);
